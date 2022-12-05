728967e5bd4f236baa8165a59a7c08f9210e6012
fakhriyya rahimova <fakhriyyarahimova@fakhriyyas-MBP.home>
2022-12-05 15:02:57 +0100

    first react exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses =[
+    { id: 'e1',title: 'car insurance', amount: 289.9, date: new Date( 2021, 2, 28)},
+    { id: 'e2',title: 'toilet papaer', amount: 45, date: new Date( 2020, 2, 28)},
+    { id: 'e3',title: 'newdesk', amount: 459, date: new Date( 2021, 2, 8)},
+    { id: 'e4',title: 'new tv', amount: 799.40, date: new Date( 2020, 5, 28)},
+  ]
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <h2>lets get started</h2>
+     <Expenses items = {expenses}/>
+  
+   </div>
+
   );
 }
 
 export default App;
+  
